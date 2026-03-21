e0a15e06e692ff7748d46f79fd589177059cada5
IzzyKnight-Nyx <ims3502@rit.edu>
2026-03-21 15:19:59 -0400

    mimic set up

---
+++ RollYourFate/Assets/Scripts/Mimic.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Mimic : Enemy
+{
+    private bool battleMode = false;
+
+    private GameObject itemDrop;
+
+    /// <summary>
+    /// Can not attack until interaced with
+    /// </summary>
+    protected override void Attack()
+    {
+        if (battleMode)
+        {
+            base.Attack();
+        }
+    }
+
+    /// <summary>
+    /// Stationary 
+    /// </summary>
+    protected override void Move()
+    {
+        return;
+    }
+
+    /// <summary>
+    /// Can not take damage until interacted with
+    /// </summary>
+    /// <param name="damage"></param>
+    public override void TakeDamage(float damage)
+    {
+        if (battleMode)
+        {
+            base.TakeDamage(damage);
+        }
+    }
+
+    /// <summary>
+    /// Player interacted with chest enter mattle mode
+    /// </summary>
+    public void Interact()
+    {
+        battleMode = true;
+    }
+
+    private void OnDestroy()
+    {
+        // Drop item 
+    }
+}
