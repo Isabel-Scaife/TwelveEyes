839592fb52ea3aa3dedb29a372df574fdaedcdde
IzzyKnight-Nyx <ims3502@rit.edu>
2026-03-22 08:48:47 -0400

    created start scene

---
+++ RollYourFate/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void Change(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+
+    }
+}
